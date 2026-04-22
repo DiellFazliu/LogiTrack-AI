a7f7263ca62d412e9e2501514d3a51904a30981a
Arijana Braha <arijana.braha@example.com>
2026-04-22 16:17:50 +0200

    jwt.strategy

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,30 +1,28 @@
-import { ExtractJwt, Strategy } from 'passport-jwt'; 
-import { PassportStrategy } from '@nestjs/passport'; 
-import { Injectable, UnauthorizedException } from '@nestjs/common'; 
-import { ConfigService } from '@nestjs/config'; 
-import { AuthService } from '../auth.service'; 
- 
-@Injectable() 
-export class JwtStrategy extends PassportStrategy(Strategy) { 
-  constructor( 
-    private configService: ConfigService, 
-    private authService: AuthService, 
-  ) { 
-    if (!secretOrKey) { 
-      throw new Error('JWT_SECRET is not defined'); 
-    } 
-    super({ 
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(), 
-      ignoreExpiration: false, 
-      secretOrKey, 
-    }); 
-  } 
- 
-  async validate(payload: any) { 
-    const user = await this.authService.validateUserById(payload.id); 
-    if (!user) { 
-      throw new UnauthorizedException('User not found'); 
-    } 
-    return user; 
-  } 
-} 
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { AuthService } from '../auth.service';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(
+    
+    private authService: AuthService,
+    configService: ConfigService,
+  ) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.authService.validateUserById(payload.id);
+    if (!user) {
+      throw new UnauthorizedException('User not found');
+    }
+    return user;
+  }
+}
