614a6c60c6bd030aac597acf6f93b43fb1028e86
albinmaqastena <albin.maqastena@student.uni-pr.edu>
2026-05-23 21:31:33 +0200

    updating files

--- backend/src/modules/reports/reports.module.ts
+++ backend/src/modules/reports/reports.module.ts
@@ -1,71 +1,24 @@
-import { Module }
-from '@nestjs/common';
-
-import {
- TypeOrmModule
-}
-from '@nestjs/typeorm';
-
-import {
- ReportsController
-}
-from './reports.controller';
-
-import {
- ReportsService
-}
-from './reports.service';
-
-import {
- Report
-}
-from './reports.entity';
-
-import {
- Shipment
-}
-from '../shipments/shipment.entity';
-
-import {
- Driver
-}
-from '../drivers/driver.entity';
-
+// src/modules/reports/reports.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
+import { Report } from './reports.entity';
+import { Shipment } from '../shipments/shipment.entity';
+import { Driver } from '../drivers/driver.entity';
+import { Vehicle } from '../vehicles/vehicle.entity';  // Import Vehicle entity
 
 @Module({
-
- imports:[
-
-   TypeOrmModule.forFeature([
-
+  imports: [
+    TypeOrmModule.forFeature([
       Report,
-
       Shipment,
-
-      Driver
-
-   ])
-
- ],
-
- controllers:[
-
-   ReportsController
-
- ],
-
- providers:[
-
-   ReportsService
-
- ],
-
- exports:[
-
-   ReportsService
-
- ]
-
+      Driver,
+      Vehicle,   // <-- Add Vehicle entity here
+    ]),
+  ],
+  controllers: [ReportsController],
+  providers: [ReportsService],
+  exports: [ReportsService],
 })
-
 export class ReportsModule {}
